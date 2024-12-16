435d562c4fc1e301967465e036a6617f76af4dbf
Neeraj Jagga <neerajjagga26@gmail.com>
2024-12-16 18:10:59 +0530

    changed userId with username

--- Backend/routes/connection.route.js
+++ Backend/routes/connection.route.js
@@ -4,8 +4,8 @@ const {followUser, unfollowUser} = require('../controllers/connection.controller
 
 const connectionRouter = express.Router();
 
-connectionRouter.post('/follow/:userId', userAuth, followUser);
-connectionRouter.post('/unfollow/:userId', userAuth, unfollowUser);
+connectionRouter.post('/follow/@:username', userAuth, followUser);
+connectionRouter.post('/unfollow/@:username', userAuth, unfollowUser);
 
 
 module.exports = {connectionRouter}

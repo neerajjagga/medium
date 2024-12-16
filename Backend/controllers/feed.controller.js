97316ecf3387182bdcb8fd4308f979a83c25059e
Neeraj Jagga <neerajjagga26@gmail.com>
2024-12-16 18:04:41 +0530

    making feed more complex

--- Backend/controllers/feed.controller.js
+++ Backend/controllers/feed.controller.js
@@ -1,6 +1,5 @@
 const { Blog } = require("../models/blog.model");
-const { Comment } = require("../models/comment.model");
-const { User } = require("../models/user.model");
+const { getPagination } = require('../utils/pagination.utility');
 
 const getForYouFeed = async (req, res) => {
     try {
@@ -9,32 +8,24 @@ const getForYouFeed = async (req, res) => {
         const CREATOR_SAFE_DATA = "name username profileImgUrl -_id"
 
         // adding pagination
-        const DEFAULT_LIMIT = 20;
-        const page = parseInt(req.query.page) || 1;
-        let limit = parseInt(req.query.limit) || DEFAULT_LIMIT;
-        limit = limit > 0 && limit <= DEFAULT_LIMIT ? limit : DEFAULT_LIMIT;
-        const skip = (page - 1) * limit;
+        const { limit, skip } = getPagination(req, 20);
 
-        // if tag specified in the query
-        // +++++++++++++++++++++++++++++  PENDING
-    
         //customized feed
         const userCustomizedBlogs = await Blog.find({
             $and: [
-                { creater: { $ne: loggedInUser._id } },
+                { creator: { $ne: loggedInUser._id } },
                 { tags: { $in: loggedInUser.interestedTopics } }
             ],
         })
-            .sort({ publishAt: -1 }) // sort newest first
+            .sort({ publishAt: -1 })
             .select(BLOG_SAFE_DATA)
             .populate('creator', CREATOR_SAFE_DATA)
             .skip(skip)
             .limit(limit)
             .lean()
 
-        if (userCustomizedBlogs.length === 0) {
-            // mixed blogs
-            const mixedGenereBlogs = await Blog.find({
+        const fetchMixedGenereBlogs = async (skip, limit) => {
+            return await Blog.find({
                 creator: { $ne: loggedInUser._id },
                 tags: { $nin: loggedInUser.interestedTopics },
             })
@@ -44,16 +35,34 @@ const getForYouFeed = async (req, res) => {
                 .skip(skip)
                 .limit(limit)
                 .lean()
+        }
+
+        if (userCustomizedBlogs.length > 0 && userCustomizedBlogs.length < limit) {
+            const pendingNoOfBlogs = limit - userCustomizedBlogs.length;
+            const pendingMixedGenereBlogs = await fetchMixedGenereBlogs(skip, pendingNoOfBlogs);
+
+            // after that concat both usercustomized field and mixedgenere fields
+            const mixedBlogs = userCustomizedBlogs.concat(pendingMixedGenereBlogs);
 
             return res.status(200).json({
                 message: "Success",
-                mixedGenereBlogs
+                blogs : mixedBlogs
+            })
+        }
+
+        if (userCustomizedBlogs.length === 0) {
+            // mixed blogs
+            const mixedGenereBlogs = await fetchMixedGenereBlogs(skip, limit);
+
+            return res.status(200).json({
+                message: "Success",
+                blogs : mixedGenereBlogs
             })
         }
 
         res.status(200).json({
             message: "Success",
-            userCustomizedBlogs
+            blogs : userCustomizedBlogs
         })
 
     } catch (error) {
@@ -65,4 +74,55 @@ const getForYouFeed = async (req, res) => {
     }
 }
 
-module.exports = { getForYouFeed };
+const getTagRelatedFeed = async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const BLOG_SAFE_DATA = "title titleSlug subtitle thumbnail clapCount postResponseCount readingTime creator publishAt -_id";
+        const CREATOR_SAFE_DATA = "name username profileImgUrl -_id"
+
+        // adding pagination
+        const { limit, skip } = getPagination(req, 20);
+
+        // if tag specified in the query
+        const tag = req.query.tag;
+
+        if (!tag) {
+            return res.status(400).json({
+                message: "Tag parameter is missing"
+            })
+        }
+
+        const tagRelatedBlogs = await Blog.find({
+            $and: [
+                { creator: { $ne: loggedInUser._id } },
+                { tags: { $in: tag } }
+            ]
+        })
+            .sort({ publishAt: -1 })
+            .select(BLOG_SAFE_DATA)
+            .populate('creator', CREATOR_SAFE_DATA)
+            .skip(skip)
+            .limit(limit)
+            .lean()
+
+        if (tagRelatedBlogs.length === 0) {
+            return res.status(404).json({
+                message: `Blogs not found with ${tag} tag`
+            })
+        }
+
+        return res.status(200).json({
+            message: "Blogs fetched successfully",
+            blogs : tagRelatedBlogs,
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Failed to fetch blogs",
+            Error: error.message
+        })
+    }
+}
+
+module.exports = { getForYouFeed, getTagRelatedFeed };

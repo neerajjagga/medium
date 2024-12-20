16661f473792ad1fbfab54b6589fe8960842cb37
Neeraj Jagga <neerajjagga26@gmail.com>
2024-12-20 13:45:26 +0530

    add topics data

---
+++ Backend/data/topics.js
@@ -0,0 +1,19 @@
+
+const topics = [
+    "Programming", "Data Science", "Technology", "Self Improvement", "Writing", "Relationships", 
+    "Machine Learning", "Productivity", "Politics", "Cryptocurrency", "Psychology", "Money", 
+    "Business", "Python", "Health", "Science", "Mental Health", "Life", "Software Development", 
+    "Startup", "Design", "JavaScript", "Artificial Intelligence", "Culture", "Software Engineering", 
+    "Blockchain", "Coding", "Entrepreneurship", "React", "UX", "Education", "History", "Humor", 
+    "Web Development", "Work", "Lifestyle", "Society", "Deep Learning", "Marketing", "Books", 
+    "NFT", "Social Media", "Leadership", "Android", "Apple", "Women", "Mindfulness", "Security", 
+    "Fitness", "AWS", "Flutter", "UX Design", "Spirituality", "Creativity", "Node.js", "UI", 
+    "DeFi", "This Happened To Me", "Philosophy", "NLP", "Economics", "World", "DevOps", "Ethereum", 
+    "Product Management", "Data Visualization", "Feminism", "Equality", "Freelancing", "Climate Change", 
+    "Bitcoin", "iOS", "Future", "Cybersecurity", "Nonfiction", "True Crime", "Web3", "Java", 
+    "Religion", "Art", "Family", "Fiction", "Kubernetes", "Parenting", "Travel", "Venture Capital", 
+    "Poetry", "Food", "Typescript", "Language", "Gaming", "Space", "Sports", "Media", "Docker", 
+    "Race", "Math", "Photography", "Music", "Justice", "Data Engineering", "Film", "Angular"
+];
+
+module.exports = {topics}

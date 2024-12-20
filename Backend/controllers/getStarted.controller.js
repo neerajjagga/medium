fa62f06e40a9d628baaab920ee2f4e9edfedb0ed
Lavit Saini <lavitsaini04@gmail.com>
2024-12-20 22:48:35 +0530

    fix merge conflict

--- Backend/controllers/getStarted.controller.js
+++ Backend/controllers/getStarted.controller.js
@@ -3,25 +3,12 @@ const { User } = require("../models/user.model");
 const { topics } = require("../data/topics");
 
 const getTopics = async (req, res) => {
-<<<<<<< HEAD
-    try {
-        res.status(200).json({ 
-            success : true,
-            message : "Topics fetched successfully",
-            topicsData: topics 
-        });
-    } catch (error) {
-        console.log("Error coming while sending topics" + error);
-        res.status(500).json({
-            success: false,
-            message: "Topics not fetched",
-            Error : error.message,
-        });
-    }
-}
-=======
   try {
-    res.status(200).json({ topicsData: topics });
+    res.status(200).json({
+      success: true,
+      message: "Topics fetched successfully",
+      topicsData: topics,
+    });
   } catch (error) {
     console.log("Error coming while sending topics" + error);
     res.status(500).json({
@@ -30,91 +17,41 @@ const getTopics = async (req, res) => {
     });
   }
 };
->>>>>>> e6d5e83 (Frontend Added!)
 
 const saveTopics = async (req, res) => {
   try {
     const loggedInUser = req.user;
     const { selectedInterestedTopics } = req.body;
 
-    // validate selectedInterestedTopics
-
-<<<<<<< HEAD
-        // check if selectedInterestedTopics is not an array 
-        if (!Array.isArray(selectedInterestedTopics)) {
-            return res.status(400).json({
-                success : false,
-                message: "selectedInterestedTopics should be an array"
-            });
-        }
-
-        // validate length
-        if (!selectedInterestedTopics || selectedInterestedTopics.length === 0) {
-            return res.status(400).json({
-                success : false,
-                message: "selectedInterestedTopics array is required"
-            });
-        }
-
-        // validate length of interestedTopics
-        if (selectedInterestedTopics.length < 3) {
-            return res.status(400).json({
-                success: false,
-                message: "Please select at least 3 topics to proceed. This helps us provide relevant content."
-            })
-        }
-
-        // slugify all selected topics
-        const sluggedSelectedInterestedTopics = selectedInterestedTopics.map(topic => slugify(topic, { lower: true, strict: true }));
-
-        // find and update user interestedTopics 
-        await User.findByIdAndUpdate({ _id: loggedInUser._id }, {
-            $addToSet: { interestedTopics: { $each: sluggedSelectedInterestedTopics } },
-            isSelectedTopics: true,
-        }, { new: true });
-
-        res.status(200).json({
-            success: true,
-            message: "Your selected topics have been added successfully"
-        })
-
-    } catch (error) {
-        console.log("Error coming while saving interested topics" + error);
-        res.status(500).json({
-            success: false,
-            message: "An error occurred while saving your selected topics. Please try again later.",
-            Error: error.message,
-        })
-=======
-    // check if selectedInterestedTopics is not an array
+    // Validate selectedInterestedTopics
     if (!Array.isArray(selectedInterestedTopics)) {
       return res.status(400).json({
+        success: false,
         message: "selectedInterestedTopics should be an array",
       });
->>>>>>> e6d5e83 (Frontend Added!)
     }
 
-    // validate length
     if (!selectedInterestedTopics || selectedInterestedTopics.length === 0) {
       return res.status(400).json({
+        success: false,
         message: "selectedInterestedTopics array is required",
       });
     }
 
-    // validate length of interestedTopics
     if (selectedInterestedTopics.length < 3) {
       return res.status(400).json({
         success: false,
-        message: "Select at least 3 topics",
+        message:
+          "Please select at least 3 topics to proceed. This helps us provide relevant content.",
       });
     }
 
-    // slugify all selected topics
+    // Slugify all selected topics
     const sluggedSelectedInterestedTopics = selectedInterestedTopics.map(
       (topic) => slugify(topic, { lower: true, strict: true })
     );
 
-    // find and update user interestedTopics
+    // Find and update user's interestedTopics
     const updatedUser = await User.findByIdAndUpdate(
       { _id: loggedInUser._id },
       {
@@ -129,7 +66,7 @@ const saveTopics = async (req, res) => {
     res.status(200).json({
       user: updatedUser,
       success: true,
-      message: "Interested topics added successfully",
+      message: "Your selected topics have been added successfully",
     });
   } catch (error) {
     console.log("Error coming while saving interested topics" + error);
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const bcrypt = require('bcrypt');
-=======
 const bcrypt = require("bcrypt");
->>>>>>> e6d5e83 (Frontend Added!)
 const { User } = require("../models/user.model");
 const {
   validateSignupData,
@@ -14,63 +10,22 @@ const signupUser = async (req, res) => {
     validateSignupData(req);
     const { name, username, emailId, password, bio } = req.body;
 
-    // find if a user already exists with this username and password;
+    // Check if a user already exists with this email or username
     const isUserPresent = await User.findOne({
       $or: [{ emailId: emailId }, { username: username }],
     });
 
-<<<<<<< HEAD
-        if (isUserPresent) {
-            return res.status(409).json({
-                success : false,
-                message: "A user already exists with this email or username",
-            });
-        }
-
-        // hash password
-        const hassedPassword = bcrypt.hashSync(password, 10);
-
-        // create new user and save 
-        const user = new User({
-            name,
-            username,
-            emailId,
-            password: hassedPassword,
-            bio,
-            profileImgUrl,
-        });
-
-        await user.save();
-
-        // generate token
-        const token = await user.getToken();
-        res.cookie('token', token, { httpOnly: true, sameSite: 'strict' });
-
-        res.status(201).json({
-            success : true,
-            message: "User created successfully",
-        });
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success : false,
-            message: "Error occurred while creating user",
-            Error: error.message
-        })
-=======
     if (isUserPresent) {
-      return res.status(400).json({
-        message: "A user already exists with this email or username!",
+      return res.status(409).json({
         success: false,
+        message: "A user already exists with this email or username.",
       });
->>>>>>> e6d5e83 (Frontend Added!)
     }
 
-    // hash password
+    // Hash password
     const hassedPassword = bcrypt.hashSync(password, 10);
 
-    // create new user and save
+    // Create new user and save
     const user = new User({
       name,
       username,
@@ -81,7 +36,7 @@ const signupUser = async (req, res) => {
 
     await user.save();
 
-    // generate token
+    // Generate token
     const token = await user.getToken();
     res.cookie("token", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, // Expires in 7 days
@@ -91,12 +46,15 @@ const signupUser = async (req, res) => {
     });
 
     res.status(201).json({
-      message: "User created successfully",
-      user: { ...user._doc, password: undefined },
       success: true,
+      message: "User created successfully",
+      user: { ...user._doc, password: undefined }, // Exclude password from response
     });
   } catch (error) {
-    res.status(500).json({ message: error.message, success: false });
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
 
@@ -105,110 +63,31 @@ const userLogin = async (req, res) => {
     validateLoginData(req);
     const { emailId, password, username } = req.body;
 
-    // check if email or username exits or not
+    // Check if email or username exists
     const user = await User.findOne({
       $or: [{ username }, { emailId }],
     });
 
-<<<<<<< HEAD
-        if (!user) {
-            return res.status(401).json({
-                success : false,
-                message: "Invalid credentials"
-            })
-        }
-        // mathch password
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-
-        if (!isPasswordValid) {
-            return res.status(401).json({
-                success : false,
-                message: `Invalid credentials`
-            })
-        }
-        // if not generate jwt based on username and id
-        const token = await user.getToken();
-
-        res.cookie('token', token, { httpOnly: true, sameSite: 'strict' });
-        res.status(200).json({
-            success : true,
-            message: "Login successfull"
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            success : false,
-            message: "Error coming while logging user",
-            Error: error.message
-        })
-=======
     if (!user) {
       return res.status(401).json({
-        message: "Invalid credentials",
         success: false,
+        message: "Invalid credentials",
       });
->>>>>>> e6d5e83 (Frontend Added!)
     }
-    // mathch password
+
+    // Match password
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
       return res.status(401).json({
-        message: `Invalid credentials`,
         success: false,
+        message: "Invalid credentials",
       });
     }
-    // if not generate jwt based on username and id
+
+    // Generate JWT token
     const token = await user.getToken();
-
-    res.cookie("token", token);
-    res.status(200).json({
-      message: "Login successfull",
-      user: { ...user._doc, password: undefined },
-      success: true,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message, success: false });
-  }
-};
-
-const userLogout = async (req, res) => {
-<<<<<<< HEAD
-    try {
-
-        const { token } = req.cookies;
-        if (!token) {
-            return res.status(400).json({
-                success : false,
-                message: "You are already logged out"
-            })
-        }
-
-        res.clearCookie('token', { httpOnly: true, sameSite: 'strict' });
-
-        res.status(200).json({
-            success : true,
-            message: "Logout successfull"
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            success : false,
-            message: "Error coming while logging out user",
-            Error: error.message
-        })
-=======
-  try {
-    const { token } = req.cookies;
-    if (!token) {
-      return res.status(400).json({
-        message: "You were already logout",
-        success: false,
-      });
->>>>>>> e6d5e83 (Frontend Added!)
-    }
-
-    res.clearCookie("token", {
+    res.cookie("token", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, // Expires in 7 days
       httpOnly: true,
       sameSite: "strict",
@@ -216,11 +95,43 @@ const userLogout = async (req, res) => {
     });
 
     res.status(200).json({
-      message: "Logout successfull",
       success: true,
+      message: "Login successful",
+      user: { ...user._doc, password: undefined }, // Exclude password from response
     });
   } catch (error) {
-    res.status(400).json({ message: error.message, success: false });
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const userLogout = async (req, res) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(400).json({
+        success: false,
+        message: "You are already logged out.",
+      });
+    }
+
+    res.clearCookie("token", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logout successful",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
 
@@ -228,7 +139,11 @@ const checkAuth = (req, res) => {
   try {
     return res.status(200).json(req.user);
   } catch (error) {
-    res.status(400).json({ message: error.message, success: false });
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
 

bb17972a484d46606fcd1d7083c698f02e26a130
Neeraj Jagga <neerajjagga26@gmail.com>
2024-12-20 13:45:16 +0530

    removed interestedTopics validation while creating user

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,26 +1,25 @@
 const bcrypt = require('bcrypt');
 const slugify = require('slugify');
-const {User} = require("../models/user.model");
-const {validateSignupData, validateLoginData} = require("../utils/userValidation");
+const { User } = require("../models/user.model");
+const { validateSignupData, validateLoginData } = require("../utils/userValidation");
 
 
-const signupUser = async(req, res) => {
+const signupUser = async (req, res) => {
     try {
         validateSignupData(req);
-        const {name, username, emailId, password, bio,  profileImgUrl, interestedTopics} = req.body;
+        const { name, username, emailId, password, bio, profileImgUrl } = req.body;
 
-        const slugifyInterestedTopics = interestedTopics.map(topic => slugify(topic, { lower: true, strict: true}));
-        
         // find if a user already exists with this username and password;
         const isUserPresent = await User.findOne({
-            $or : [
-                {emailId : emailId},
-                {username : username}
+            $or: [
+                { emailId: emailId },
+                { username: username }
             ]
         });
-        if(isUserPresent) {
+
+        if (isUserPresent) {
             return res.status(409).json({
-                message : "A user already exists with this email or username",
+                message: "A user already exists with this email or username",
             });
         }
 
@@ -29,29 +28,28 @@ const signupUser = async(req, res) => {
 
         // create new user and save 
         const user = new User({
-            name, 
-            username, 
-            emailId, 
-            password : hassedPassword, 
-            bio, 
+            name,
+            username,
+            emailId,
+            password: hassedPassword,
+            bio,
             profileImgUrl,
-            interestedTopics : slugifyInterestedTopics
         });
 
         await user.save();
 
         // generate token
-        const token = await user.getToken(); 
+        const token = await user.getToken();
         res.cookie('token', token, { httpOnly: true, sameSite: 'strict' });
 
         res.status(201).json({
-            message : "User created successfully",
+            message: "User created successfully",
         })
     } catch (error) {
         console.log(error);
         res.status(300).json({
-            message : "Error coming while creating user",
-            Error : error.message
+            message: "Error coming while creating user",
+            Error: error.message
         })
     }
 }
@@ -59,41 +57,41 @@ const signupUser = async(req, res) => {
 const userLogin = async (req, res) => {
     try {
         validateLoginData(req);
-        const {emailId, password, username} = req.body;
+        const { emailId, password, username } = req.body;
 
         // check if email or username exits or not
         const user = await User.findOne({
-            $or : [
-                {username},
-                {emailId}
+            $or: [
+                { username },
+                { emailId }
             ]
         });
 
-        if(!user) {
+        if (!user) {
             return res.status(401).json({
-                message : "Invalid credentials"
+                message: "Invalid credentials"
             })
         }
         // mathch password
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
-        if(!isPasswordValid) {
+        if (!isPasswordValid) {
             return res.status(401).json({
                 message: `Invalid credentials`
             })
         }
         // if not generate jwt based on username and id
-        const token = await user.getToken(); 
+        const token = await user.getToken();
 
         res.cookie('token', token, { httpOnly: true, sameSite: 'strict' });
         res.status(200).json({
-            message : "Login successfull"
+            message: "Login successfull"
         })
 
     } catch (error) {
         res.status(400).json({
-            message : "Error coming while logging user",
-            Error : error.message
+            message: "Error coming while logging user",
+            Error: error.message
         })
     }
 }
@@ -101,25 +99,25 @@ const userLogin = async (req, res) => {
 const userLogout = async (req, res) => {
     try {
 
-        const {token} = req.cookies;
-        if(!token) {
+        const { token } = req.cookies;
+        if (!token) {
             return res.status(400).json({
-                message : "You were already logout"
+                message: "You were already logout"
             })
         }
 
         res.clearCookie('token', { httpOnly: true, sameSite: 'strict' });
 
         res.status(200).json({
-            message : "Logout successfull"
+            message: "Logout successfull"
         })
 
     } catch (error) {
         res.status(400).json({
-            message : "Error coming while logging out user",
-            Error : error.message
+            message: "Error coming while logging out user",
+            Error: error.message
         })
     }
 }
 
-module.exports = {signupUser, userLogin, userLogout};
+module.exports = { signupUser, userLogin, userLogout };

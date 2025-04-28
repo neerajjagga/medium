93013a68f3dd80bd42a540ac9d0fd7202336576e
Neeraj Jagga <neerajjagga26@gmail.com>
2025-04-28 10:49:14 +0530

    fixed some deploy issues

--- Backend/app.js
+++ Backend/app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 // app.use(limiter);
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://medium-git-main-neerajjaggas-projects.vercel.app"],
     credentials: true,
   })
 );
